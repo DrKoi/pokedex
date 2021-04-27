797d272446f45c811dd029347a4a076d018939f9
Kevin Figueroa <keenrivoj@gmail.com>
2021-04-27 03:28:02 -0400

    j

--- js/index.js
+++ js/index.js
@@ -16,11 +16,50 @@ tinymce.init({
     "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
 });
 
+const pokemones = [];
+const cargarTabla = ()=>{
+  
+  const tbody = document.querySelector("#tbody-tabla");
+
+  //tbody.innerHTML = 
+  for (let i = 0; i < pokemones.length; ++i){
+    let p = pokemons[i];
+    let tr = document.createElement("tr");
+
+    let tdNro = document.createElement("td");
+    let tdNombre = document.createElement("td");
+    let tdTipo = document.createElement("td");
+    let tdDescripcion = document.createElement("td");
+    let tdAcciones = document.createElement("td");
+    
+    tdNro.innerText = i + 1;
+    tdNombre.innerText = p.nombre;
+    tdTipo.innerText = p.tipo;
+    tdDescripcion.innerText = p.Descripcion;
+    tdAcciones.innerText = p.acciones;
+
+    tr.appendChild(tdNro);
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdDescripcion);
+    tr.appendChild(tdTipo);
+    
+
+    tbody.appendChild(tr);
+  }
+};
 document.querySelector("#registrar-btn").addEventListener("click", () => {
   let nombre = document.querySelector("#nombre-txt").value;
   let tipo = document.querySelector("#tipo-select").value;
   let legendario = document.querySelector("#legendario-si").checked;
   let descripcion = tinymce.get("descripcion-txtarea").getContent();
 
-  console.log("Hola Mundo", nombre, tipo, legendario, descripcion);
+  let pokemon = {};
+
+  pokemon.nombre = nombre;
+  pokemon.tipo = tipo;
+  pokemon.legendario = legendario;
+  pokemon.descripcion = descripcion;
+
+  pokemones.push(pokemon);
+  Swal.fire("Resultado exitoso!", "Pokemon registrado", "success");
 });
